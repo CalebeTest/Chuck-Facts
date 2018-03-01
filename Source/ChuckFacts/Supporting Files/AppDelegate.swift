5bbd50395b79f3d156c595c9f8c6d63060c2d01c
Calebe Emerick <cemerik@stone.com.br>
2018-03-01 17:01:15 -0300

    Cria a estrutura inicial do projeto.

---
+++ Source/ChuckFacts/Supporting Files/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  ChuckFacts
+//
+//  Created by Calebe Emerick on 01/03/2018.
+//  Copyright © 2018 Stone Pagamentos. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+	
+	var window: UIWindow?
+	
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+		
+		
+		return true
+	}
+}
